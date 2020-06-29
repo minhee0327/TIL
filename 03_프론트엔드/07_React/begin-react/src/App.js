b2708d888b5987fcbe8b30cc68fbb3262951262e
minhee <queen.minhee@gmail.com>
2020-06-29 16:56:30 +0900

    [ADD] React: review (basic-react)

---
+++ 03_프론트엔드/07_React/begin-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './Hello.js';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
