bed3fcb02a473be80648a8e63dd269f808f7bb2f
minhee <queen.minhee@gmail.com>
2020-07-01 00:25:39 +0900

    [ADD] React: useState, useRef로 이벤트 관리하기

--- 03_프론트엔드/07_React/begin-react/src/App.js
+++ 03_프론트엔드/07_React/begin-react/src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import Hello from './Component/Hello.js';
-import Wrapper from './Component/Wrapper.js'
 import './App.css'
+import InputSample from './Component/InputSample';
 
 function App() {
-  const name = "react";
-  //1. 인라인 스타일 작성방법
-  // naming할때 Camel 표기법으로 naming한다.
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
-
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial />
-      <Hello color='blue' />
-      {/* 1. inline 선언방식 */}
-      <div style={style}>{name}</div>
-      {/* 2. class 선언방식 : className으로 설정*/}
-      <div className="gray-box"></div>
-    </Wrapper>
+    <InputSample />
   );
 }
 
