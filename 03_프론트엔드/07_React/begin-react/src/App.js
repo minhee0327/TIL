42fc78f605b64f86ccbac4485db0d79008408b40
minhee <queen.minhee@gmail.com>
2020-07-06 23:53:25 +0900

    [Update] immer 로 불변성 쉽게 관리하기

--- 03_프론트엔드/07_React/begin-react/src/App.js
+++ 03_프론트엔드/07_React/begin-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useMemo } from 'react';
 import UserList from './Component/UserList';
 import CreateUser from './Component/CreateUser';
+import produce from 'immer';
 
 function countAcitveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -37,20 +38,19 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      }
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      })
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user => user.id === action.id ? { ...user, active: !user.active } : user)
-      }
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      })
     default:
       return state;
   }
