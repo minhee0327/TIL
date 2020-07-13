28036353b3ca123019cbab8a86e18b9a22082477
minhee <queen.minhee@gmail.com>
2020-07-13 21:24:11 +0900

    [ADD] C: 함수포인터, 구조체, 파일입출력

--- 01_컴퓨터공학/04_C,C++,C#,자료구조/C_basic/Project1/Project1/main.c
+++ 01_컴퓨터공학/04_C,C++,C#,자료구조/C_basic/Project1/Project1/main.c
@@ -1,8 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char name[20];
+	int score;
+} Student;
 
 int main(void) {
+	int n, sum = 0;
+	FILE* fp;
 
+	fp = fopen("input.txt", "r");
+	fscanf(fp, "%d", &n);
+	Student* students = (Student*)malloc(sizeof(Student) * n);
+	for (int i = 0; i < n; i++) {
+		fscanf(fp, "%s %d", &((students + i)->name), &((students + i)->score));
+	}
+	for (int i = 0; i < n; i++) {
+		sum += (students + i)->score;
+	}
+	free(students);
+	fclose(fp);
+	printf("Á¡¼ö Æò±Õ: %.2f\n", (double)sum / n);
 	system("pause");
 	return 0;
 }
