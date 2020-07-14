afd4c9993d5067b9b59659d639d6e3d6f13c15ce
minhee <queen.minhee@gmail.com>
2020-07-14 18:04:15 +0900

    [ADD] C: 전처리기 (기본 C언어 마무리)

--- 01_컴퓨터공학/04_C,C++,C#,자료구조/C_basic/Project1/Project1/main.c
+++ 01_컴퓨터공학/04_C,C++,C#,자료구조/C_basic/Project1/Project1/main.c
@@ -1,29 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct {
-	char name[20];
-	int score;
-} Student;
+#include "17_example01.h"
+#include "17_example01.h"
 
 int main(void) {
-	int n, sum = 0;
-	FILE* fp;
 
-	fp = fopen("input.txt", "r");
-	fscanf(fp, "%d", &n);
-	Student* students = (Student*)malloc(sizeof(Student) * n);
-	for (int i = 0; i < n; i++) {
-		fscanf(fp, "%s %d", &((students + i)->name), &((students + i)->score));
-	}
-	for (int i = 0; i < n; i++) {
-		sum += (students + i)->score;
-	}
-	free(students);
-	fclose(fp);
-	printf("Á¡¼ö Æò±Õ: %.2f\n", (double)sum / n);
+	printf("%d\n",add(3,7));
 	system("pause");
 	return 0;
 }
