145f22006c3b682a42078a8b417fe8fde346079b
minhee <queen.minhee@gmail.com>
2020-07-29 12:09:10 +0900

    [ADD] Programmers Lv1: 문자열을 정수로바꾸기 (javaScript)

---
+++ 02_알고리즘/Programmers/lv1_문자열을 정수로바꾸기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  return s * 1;
+}
+
+/*
+- str to int
+    - 숫자 곱
+    - parseInt(string, radix), strToInt(), Number() 내장함수
+    - 
+
+- int to str => +""
+    - String()
+
+*/
