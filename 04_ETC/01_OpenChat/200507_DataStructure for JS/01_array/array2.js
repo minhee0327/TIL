65893dbac0b2c3242cacab96ae366cbc40e63530
minhee <queen.minhee@gmail.com>
2020-08-04 19:34:04 +0900

    [UPDATE] 폴더구조 정리

--- 04_ETC/01_OpenChat/200507_DataStructure for JS/01_array/array2.js
+++
@@ -1,8 +0,0 @@
-// filter() 및 람다 사용하여 짝수 함수 구하기
-// 시간복잡도는 array1.js와 동일하다.
-
-function removeEven(arr) {
-    return arr.filter(v => v % 2 != 0)
-}
-
-console.log(removeEven([1, 2, 4, 5, 10, 6, 3]))
