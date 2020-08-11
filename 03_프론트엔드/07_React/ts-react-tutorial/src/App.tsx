e2b08f7583b01f1a3e8c250601632a2310264467
minhee <queen.minhee@gmail.com>
2020-08-11 13:31:49 +0900

    [ADD] React & TypeScript: useReducer로 상태관리하기

--- 03_프론트엔드/07_React/ts-react-tutorial/src/App.tsx
+++ 03_프론트엔드/07_React/ts-react-tutorial/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 // import Counter from "./Counter";
-import MyForm from "./MyForm";
+// import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 function App() {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-  return <MyForm onSubmit={onSubmit} />;
+  // const onSubmit = (form: { name: string; description: string }) => {
+  //   console.log(form);
+  // };
+  return <ReducerSample />;
 }
 
 export default App;
