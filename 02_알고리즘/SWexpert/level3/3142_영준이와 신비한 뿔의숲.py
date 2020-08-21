9ef8bc8923547439a88ea22ff0ebc2a624f92c0a
minhee <queen.minhee@gmail.com>
2020-08-21 09:51:38 +0900

    [ADD] SWEA: D3

---
+++ 02_알고리즘/SWexpert/level3/3142_영준이와 신비한 뿔의숲.py
@@ -0,0 +1,3 @@
+for t in range(1, 1+int(input())):
+    n, m = map(int,input().split())
+    print("#{} {}".format(2*m-n, n-m))
---
+++ 02_알고리즘/SWexpert/level3/3260_두수의덧셈.py
@@ -0,0 +1,7 @@
+for t in range(1, 1+int(input())):
+    a, b = map(int, input().split())
+    print("#{} {}".format(t, a+b))
+
+'''
+..?왜 D3지..?
+'''
