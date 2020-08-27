29df51b9918f678ddf4302313d325be306392a67
minhee0327 <queen.minhee@gmail.com>
2020-08-27 14:26:02 +0900

    [ADD] 알고리즘 (수학): 등차, 등비, 계차 수열 정리

---
+++ 02_알고리즘/SWexpert/tip/01_등차수열.py
@@ -0,0 +1,26 @@
+'''
+등차수열 (Arithmetic progression)
+- n번째 항 찾기
+- 초항, 공차, 몇번째항인지 입력받기 (start, diff, n)
+- 몇번째인지 count 하기 (cnt)
+- cnt 와 n이 동일하면 해당 값 return 
+'''
+
+
+def AP(start, diff, n):
+    ans = start
+    cnt = 1
+
+    while True:
+        ans += diff
+        cnt += 1
+
+        if n == cnt:
+            return ans
+
+
+start = int(input("초항: "))
+diff = int(input("공차(공통차이값): "))
+n = int(input("몇번째항?: "))
+
+print(n, "번째 항", AP(start, diff, n))
