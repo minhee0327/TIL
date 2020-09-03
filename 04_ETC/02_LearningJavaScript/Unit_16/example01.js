b64a8f9ab8b7eb9f5e3df40bf97edf1dbe48c53f
minhee0327 <queen.minhee@gmail.com>
2020-09-03 22:40:36 +0900

    [ADD] Learning JS: Math객체

---
+++ 04_ETC/02_LearningJavaScript/Unit_16/example01.js
@@ -0,0 +1,12 @@
+const x = 1900.51;
+
+console.log(x.toFixed(3)) //1900.510
+console.log(x.toFixed(2)) //1900.51
+console.log(x.toFixed(1)) //1900.5
+console.log(x.toFixed(0)) //1901
+
+console.log(x.toExponential(3)) //1.901e+3
+console.log(x.toExponential(2)) //1.90e+3
+console.log(x.toExponential(1)) //1.9e+3
+console.log(x.toExponential(0)) //2e+3
+
