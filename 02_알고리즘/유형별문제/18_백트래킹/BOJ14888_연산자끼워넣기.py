aa6713cdecbbc2a993a469c8b735bcbb49458a65
minhee0327 <queen.minhee@gmail.com>
2020-09-22 12:30:19 +0900

    [UPDATE] BOJ - 백트래킹 200922 수정

--- 02_알고리즘/유형별문제/18_백트래킹/BOJ14888_연산자끼워넣기.py
+++ 02_알고리즘/유형별문제/18_백트래킹/BOJ14888_연산자끼워넣기.py
@@ -1,3 +1,39 @@
+def recursive(idx, ans, add, sub, mul, div):
+    global mx, mn
+
+    if idx == N:
+        mx = max(mx, ans)
+        mn = min(mn, ans)
+        return
+
+    if add:
+        recursive(idx+1, ans + A[idx], add-1, sub, mul, div)
+    if sub:
+        recursive(idx+1, ans - A[idx], add, sub-1, mul, div)
+    if mul:
+        recursive(idx+1, ans * A[idx], add, sub, mul-1, div)
+    if div:
+        if ans > 0:
+            recursive(idx+1, ans // A[idx], add, sub, mul, div-1)
+        else:
+            recursive(idx+1, -1 * ((ans*-1) // A[idx]), add, sub, mul, div-1)
+
+
+N = int(input())
+A = list(map(int, input().split()))
+op = list(map(int, input().split()))
+mx, mn = -1000000000, 1000000000
+
+recursive(1, A[0], op[0], op[1], op[2], op[3])
+print("{}\n{}".format(mx, mn))
+'''
+- 위코드는 찾아서 보고 따라한 코드.
+- 일단 재귀적으로 완전 탐색 접근을 못했었는데, 재귀적으로 탐색을 해나가는 것을 배웠다.
+'''
+
+
+'''
+# 시간초과로 다시 풀어보기!
 from itertools import permutations
 
 
@@ -45,6 +81,4 @@ for p in possibiliyt_op:
 print(max_v)
 print(min_v)
 
-'''
-- 시간초과로 다시 풀어보기!
 '''
