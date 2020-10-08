5d0677528e1cba3b6431cdbde0188d0e7a0158be
minhee0327 <queen.minhee@gmail.com>
2020-10-08 11:58:14 +0900

    [UPDATE] 알고리즘 - 201008 [구현]

--- 02_알고리즘/유형별문제/01_기본/BOJ1244_스위치켜고끄기.py
+++ 02_알고리즘/유형별문제/01_기본/BOJ1244_스위치켜고끄기.py
@@ -1,33 +1,37 @@
 N = int(input())
 switch = list(map(int, input().split()))
 
-# 학생수만큼 반복해서, 성별과 스위치의 번호를 받음.
 for _ in range(int(input())):
     S, num = map(int, input().split())
-    # 성별 남
     if S == 1:
-        for i in range(num-1, N, num):
-            if switch[i] == 1:
-                switch[i] = 0
-            elif switch[i] == 0:
-                switch[i] = 1
+        k = num - 1
+        while k < N:
+            switch[k] = 1 - switch[k]
+            k += num
+    else:
+        switch[num-1] = 1 - switch[num-1]
 
-    # 성별 여
-    elif S == 2:
-        start, end = num-2, num
-        while start > -1 and end <= N:
-            print(start, end)
-            if switch[start] != switch[end]:
+        for i in range(1, N-1):
+            if num - 1 - i >= 0 and num - 1 + i < N and switch[num-1-i] == switch[num-1+i]:
+                switch[num-1-i] = 1 - switch[num-1-i]
+                switch[num-1+i] = 1 - switch[num-1+i]
+            else:
                 break
 
-            elif switch[start] == switch[end] == 0:
-                switch[start] = 1
-                switch[end] = 1
-            elif switch[start] == switch[end] == 1:
-                switch[start] = 0
-                switch[end] = 0
-            start -= 1
-            end += 1
-    print(switch)
+for i, e in enumerate(switch):
+    if i and not (i % 20):
+        print()
+    print(e, end=" ")
 
-print(' '.join(map(str, switch)))
+
+'''
+코드 수정
+- 과정1
+    - switch[k] = 1 - switch[k]
+    - 원래는 나머지연산으로 계산했었다가
+    - 반복문으로 해봤다가, 위 코드가 가장 깔끔한 것 같아 수정
+- 과정 2
+    - 출력부분, enumerate로 idx값 받아서 활용함
+- 과정 3
+    - index가 0보다 작아졌을경우와 N보다 커졌을 경우 그 부분을 한칸씩 더 줄였다.
+'''
