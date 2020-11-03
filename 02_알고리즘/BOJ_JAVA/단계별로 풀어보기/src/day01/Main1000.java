72d73e3e66218c60cf1b5153d0e17faaf6de4c47
minhee0327 <queen.minhee@gmail.com>
2020-11-03 22:35:04 +0900

    [ADD] 알고리즘 기본입출력 및 사칙연산 (java)

---
+++ 02_알고리즘/BOJ_JAVA/단계별로 풀어보기/src/day01/Main1000.java
@@ -0,0 +1,15 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Main1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + b);
+	}
+}
---
+++ 02_알고리즘/BOJ_JAVA/단계별로 풀어보기/src/day01/Main10171.java
@@ -0,0 +1,7 @@
+package day01;
+
+public class Main10171 {
+	public static void main(String[]args) {
+		System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+	}
+}
