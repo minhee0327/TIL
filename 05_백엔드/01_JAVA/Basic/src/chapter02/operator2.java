f43d8f0940e4f6b20738ae89c969f4f679565907
minhee0327 <queen.minhee@gmail.com>
2020-11-05 22:55:00 +0900

    [ADD] JAVA - chapter 02 - operand

---
+++ 05_백엔드/01_JAVA/Basic/src/chapter02/operator2.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class operator2 {
+
+	public static void main(String[] args) {
+		int num = 10 % 3;
+		System.out.println(num);
+
+		int num2 = 10;
+		num2 += 2;
+		System.out.println(num2);
+	}
+}
---
+++ 05_백엔드/01_JAVA/Basic/src/chapter02/operator5.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class operator5 {
+
+	public static void main(String[] args) {
+//		삼항연산자
+		int num1 = 10;
+		int num2 = 20;
+	
+		int max = (num1 > num2) ? num1: num2;
+		System.out.println(max);
+	}
+}
